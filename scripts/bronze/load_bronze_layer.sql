18d22965ba48a0b7750f8634aad79deb9b83039d
Ameya Gurjar <76804848+Ameya674@users.noreply.github.com>
2025-09-13 15:27:02 -0700

    Create load_bronze_layer.sql

---
+++ scripts/bronze/load_bronze_layer.sql
@@ -0,0 +1,78 @@
+-- ===============================================================
+-- THE FOLLWLING CODE LOADS DATA INTO THE TABLES IN BRONZE SCHEMA
+-- ===============================================================
+
+CREATE OR ALTER PROCEDURE bronze.load_bronze AS
+BEGIN
+	
+	DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME;
+
+	BEGIN TRY
+		SET @batch_start_time = GETDATE();
+		PRINT '======================================';
+		PRINT 'Loading Bronze Layer';
+		PRINT '======================================';
+
+
+		TRUNCATE TABLE bronze.ball_by_ball;
+		BULK INSERT bronze.ball_by_ball
+		FROM 'C:\Users\itadmin\Desktop\ipl_data_warehouse\Ball_By_Ball.csv'
+		WITH 
+		(
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+		)
+
+		TRUNCATE TABLE bronze.match;
+		BULK INSERT bronze.match
+		FROM 'C:\Users\itadmin\Desktop\ipl_data_warehouse\Match.csv'
+		WITH 
+		(
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+		);
+
+		TRUNCATE TABLE bronze.player;
+		BULK INSERT bronze.player
+		FROM 'C:\Users\itadmin\Desktop\ipl_data_warehouse\Player.csv'
+		WITH 
+		(
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+		);
+
+		TRUNCATE TABLE bronze.team;
+		BULK INSERT bronze.team 
+		FROM 'C:\Users\itadmin\Desktop\ipl_data_warehouse\Team.csv'
+		WITH 
+		(
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+		);
+
+		TRUNCATE TABLE bronze.player_match;
+		BULK INSERT bronze.player_match
+		FROM 'C:\Users\itadmin\Desktop\ipl_data_warehouse\Player_match.csv'
+		WITH 
+		(
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+		);
+
+	END TRY
+
+	BEGIN CATCH
+		PRINT '=========================================='
+		PRINT 'ERROR OCCURED DURING LOADING BRONZE LAYER'
+		PRINT 'Error Message: ' + ERROR_MESSAGE();
+		PRINT 'Error Message: ' + CAST (ERROR_NUMBER() AS NVARCHAR);
+		PRINT 'Error Message: ' + CAST (ERROR_STATE() AS NVARCHAR);
+		PRINT '=========================================='
+	END CATCH
+
+END
